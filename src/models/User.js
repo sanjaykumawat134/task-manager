9dfe824db8e0ad5a573e0505d64968133a7522f8
sanjay kumawat <sanjaykumawat134@gmail.com>
2021-07-16 18:36:00 +0530

    cascade delete

--- src/models/User.js
+++ src/models/User.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const Task = require("./Task")
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -90,6 +91,12 @@ userSchema.pre("save", async function (next) {
   }
   next();
 }); // middleware pre and post hook
+
+userSchema.pre("remove",async function (next){
+    const user = this;
+    await Task.deleteMany({owner:user._id});
+    next();
+});
 const User = mongoose.model("User", userSchema);
 
 module.exports = User;
